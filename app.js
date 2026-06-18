e4114f2d6648b09fd43a0a9ab0afe9a0bee586df
Danny Medrano <dmedrano@technisys.com>
2026-06-18 11:54:11 -0500

    exercise 12 restore health check

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (_request, response) => {
-  response.send('1')
+  response.send('3')
 })
 
 app.get('/health', (_request, response) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw new Error('error...')
   response.send('ok')
 })
 
